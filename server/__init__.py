574dfddee818f752d20a1ba081c4b6e640f558c6
Joakim Skogø Langvand <jlangvand@gmail.com>
2020-04-21 14:28:55 +0200

    Log to file

--- server/__init__.py
+++ server/__init__.py
@@ -14,6 +14,7 @@ Logging
 """
 
 LOG_FORMAT_STREAM = "%(asctime)s | %(levelname)s | %(message)s"
+LOG_FORMAT_FILE = LOG_FORMAT_STREAM
 LOG_FILE = "../logfile"
 LOG_LEVEL = logging.INFO
 
@@ -21,5 +22,9 @@ logger = logging.getLogger()
 stream_handler = logging.StreamHandler()
 stream_formatter = logging.Formatter(LOG_FORMAT_STREAM)
 stream_handler.setFormatter(stream_formatter)
+file_handler = logging.FileHandler('../log')
+file_formatter = logging.Formatter(LOG_FORMAT_FILE)
+file_handler.setFormatter(file_formatter)
 logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 logger.setLevel(LOG_LEVEL)
