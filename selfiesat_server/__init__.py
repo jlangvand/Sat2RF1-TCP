e5ed855ec901ce70c1762089ad44ccf39daf423c
Joakim Skogø Langvand <jlangvand@gmail.com>
2020-04-21 16:11:50 +0200

    Move config to file

--- selfiesat_server/__init__.py
+++ selfiesat_server/__init__.py
@@ -12,31 +12,6 @@ DATA_PORT = 20202
 HOSTNAME = "localhost"
 
 """
-Logging
-"""
-
-LOG_FORMAT_STREAM = "%(asctime)s | %(levelname)s | %(message)s"
-LOG_FORMAT_FILE = LOG_FORMAT_STREAM
-LOG_FILE = "../logfile"
-LOG_LEVEL = logging.INFO
-
-LOG_TO_CONSOLE = True
-
-logger = logging.getLogger()
-
-if LOG_TO_CONSOLE:
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_formatter = logging.Formatter(LOG_FORMAT_STREAM)
-    stream_handler.setFormatter(stream_formatter)
-    logger.addHandler(stream_handler)
-
-file_handler = logging.FileHandler('../log')
-file_formatter = logging.Formatter(LOG_FORMAT_FILE)
-file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
-logger.setLevel(LOG_LEVEL)
-
-"""
 Config
 """
 
@@ -44,4 +19,30 @@ with open("config.yaml", 'r') as stream:
     try:
         config = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
-        logger.warning('Error in configuration file: %s', exc)
+        logging.error('Error in configuration file: %s', exc)
+
+"""
+Logging
+"""
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+"""
+Log to file
+"""
+
+file_handler = logging.FileHandler(config['logging']['file_path'])
+file_formatter = logging.Formatter(config['logging']['file_format'])
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+"""
+Log to console
+"""
+
+if config['debug']['log_to_console']:
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_formatter = logging.Formatter(config['logging']['console_format'])
+    stream_handler.setFormatter(stream_formatter)
+    logger.addHandler(stream_handler)
