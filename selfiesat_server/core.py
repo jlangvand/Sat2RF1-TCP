9da56b1c68539a0743a424b93d2180f1d7156087
Joakim Skogø Langvand <jlangvand@gmail.com>
2020-04-21 16:52:37 +0200

    Main loop sketch

--- selfiesat_server/core.py
+++ selfiesat_server/core.py
@@ -24,6 +24,15 @@ def main():
         logger.error(e.strerror)
         if not config['debug']['fake_radio_connection']: sys.exit(1)
 
+    while True:
+        # Update sockets, allows them to receive data.
+        # Setting a timeout so they won't block execution if no data is present.
+        settings_socket.server_cycle(0.5)
+        data_socket.server_cycle(0.5)
+
+        # TODO: check for data/settings and do stuff
+
+        # TODO: check for incoming data from radio, do stuff
 
 if __name__ == '__main__':
     main()
